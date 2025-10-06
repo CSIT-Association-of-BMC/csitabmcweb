b1263db9ba5caa2df18e9a7762fde653fa31246e
Sanchit Pandey <120261834+sanchitpandeyyy@users.noreply.github.com>
2025-10-06 12:51:19 +0545

    Meta Data Modified
    Set metadata title and description for the page.

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@ import About from "./about/page";
 import Event from "./events/page";
 import HeroLander from "@/components/custom/HeroLander/HeroLander";
 import Testimonial from "@/components/custom/Testimonial";
+import { metadata } from "@/app/layout";
 import CommunityPartners from "@/components/custom/CommunityPartners";
 
 
 export default function Home() {
+   metadata.title = "CSIT Association of BMC";
+  metadata.description = "CSIT Association of Butwal Multiple Campus is Non profit and Non political IT Organization for emerging IT Students.";
   return (
     <>
     {/* <Lander/> */}
